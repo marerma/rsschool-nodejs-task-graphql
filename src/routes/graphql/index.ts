b824b87553f5160036707a01c695a73a79a4aa01
marerma <maria.kadantseva@gmail.com>
2024-03-01 23:03:00 +0300

    feat: add depthLength

--- src/routes/graphql/index.ts
+++ src/routes/graphql/index.ts
@@ -1,6 +1,9 @@
 import { FastifyPluginAsyncTypebox } from '@fastify/type-provider-typebox';
 import { createGqlResponseSchema, gqlResponseSchema, schema } from './schemas.js';
-import { graphql } from 'graphql';
+import { graphql, validate, parse } from 'graphql';
+import depthLimit from 'graphql-depth-limit';
+
+const MAX_DEPTH = 5;
 
 const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
   fastify.route({
@@ -13,15 +16,21 @@ const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
       },
     },
     async handler(req) {
+      const queryDocument = parse(req.body.query);
+
+      const validationErrors = validate(schema, queryDocument, [depthLimit(MAX_DEPTH)]);
+
+      if (validationErrors.length > 0) {
+        return { errors: validationErrors };
+      }
       return await graphql({
         schema,
         source: req.body.query,
-        contextValue: {dataBase: fastify.prisma},
-        variableValues: req.body.variables
-      })
-    }
+        contextValue: { dataBase: fastify.prisma },
+        variableValues: req.body.variables,
+      });
     },
-  );
+  });
 };
 
 export default plugin;
